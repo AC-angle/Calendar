8ae2da56d992b2a9a8e3e46bb8cd7cd72bd7e7d4
心叶 <yelloxing@gmail.com>
2020-08-04 15:03:35 +0800

    日历基本的功能完毕

--- calendar.js
+++ calendar.js
@@ -1,6 +1,11 @@
 
 // 选择天的视图
-function selectDayView(targetEl, year, month, cYear, cMonth, cDay) {
+function selectDayView(el, targetEl, year, month, cYear, cMonth, cDay) {
+
+    var today = new Date();
+    var tYear = today.getFullYear();
+    var tMonth = today.getMonth() + 1;
+    var tDay = today.getDate();
 
     // 头部分
     var template =
@@ -25,7 +30,14 @@ function selectDayView(targetEl, year, month, cYear, cMonth, cDay) {
     // 内容主体部分
     var days = getMonthDays(year, month);
     for (var i = 1; i <= days; i++) {
-        template += "<li class='click item' name='item-click'>" + i + "</li>";
+        var clazz = 'click item';
+        if (year == cYear && month == cMonth && i == cDay) {
+            clazz += " selected";
+        }
+        if (year == tYear && month == tMonth && i == tDay) {
+            clazz += " today";
+        }
+        template += "<li class='" + clazz + "' name='item-click' val='" + i + "'>" + i + "</li>";
     }
 
     // 后置灰色
@@ -44,22 +56,30 @@ function selectDayView(targetEl, year, month, cYear, cMonth, cDay) {
 
     preEl.addEventListener('click', function () {
         if (month == 1) { month = 12; year -= 1; } else { month -= 1; }
-        selectDayView(targetEl, year, month, cYear, cMonth, cDay);
+        selectDayView(el, targetEl, year, month, cYear, cMonth, cDay);
     }, false);
 
     titleEl.addEventListener('click', function () {
-        selectMonthView(targetEl, year, cYear, cMonth);
+        selectMonthView(el, targetEl, year, cYear, cMonth);
     }, false);
 
     nextEl.addEventListener('click', function () {
-        if (month == 12) { year += 1; month = 1; } else { month =  parseInt(month)+1; }
-        selectDayView(targetEl, year, month, cYear, cMonth, cDay);
+        if (month == 12) { year -= -1; month = 1; } else { month = parseInt(month) + 1; }
+        selectDayView(el, targetEl, year, month, cYear, cMonth, cDay);
     }, false);
+
+    // 点击小条目的事件
+    document.getElementsByName('item-click').forEach(function (itemNode) {
+        itemNode.addEventListener('click', function () {
+            el.value = year + '/' + month + '/' + itemNode.getAttribute('val');
+            targetEl.innerHTML = '';
+        }, false);
+    });
 
 }
 
 // 选择月的视图
-function selectMonthView(targetEl, year, cYear, cMonth, cDay) {
+function selectMonthView(el, targetEl, year, cYear, cMonth, cDay) {
 
     // 头部分
     var template =
@@ -73,7 +93,11 @@ function selectMonthView(targetEl, year, cYear, cMonth, cDay) {
 
     // 内容部分
     for (var i = 1; i <= 12; i++) {
-        template += "<li class='click item' name='item-click' val='" + i + "'>" + i + "</li>";
+        var clazz = 'click item';
+        if (year == cYear && i == cMonth) {
+            clazz += " selected";
+        }
+        template += "<li class='" + clazz + "' name='item-click' val='" + i + "'>" + i + "</li>";
     }
 
     template += "</ul></div>";
@@ -85,27 +109,27 @@ function selectMonthView(targetEl, year, cYear, cMonth, cDay) {
     var nextEl = document.getElementsByName('next')[0];
 
     preEl.addEventListener('click', function () {
-        selectMonthView(targetEl, year - 1, cYear, cMonth, cDay)
+        selectMonthView(el, targetEl, year - 1, cYear, cMonth, cDay)
     }, false);
 
     titleEl.addEventListener('click', function () {
-        selectYearView(targetEl, year, cYear, cMonth, cDay);
+        selectYearView(el, targetEl, year, cYear, cMonth, cDay);
     }, false);
 
     nextEl.addEventListener('click', function () {
-        selectMonthView(targetEl, year + 1, cYear, cMonth, cDay)
+        selectMonthView(el, targetEl, year - -1, cYear, cMonth, cDay)
     }, false);
 
     // 点击小条目的事件
     document.getElementsByName('item-click').forEach(function (itemNode) {
         itemNode.addEventListener('click', function () {
-            selectDayView(targetEl, year, itemNode.getAttribute('val'), cYear, cMonth, cDay);
+            selectDayView(el, targetEl, year, itemNode.getAttribute('val'), cYear, cMonth, cDay);
         }, false);
     });
 }
 
 // 选择年的视图
-function selectYearView(targetEl, year, cYear, cMonth, cDay) {
+function selectYearView(el, targetEl, year, cYear, cMonth, cDay) {
     var decYears = getDecYears(year);
 
     // 头部分
@@ -121,7 +145,11 @@ function selectYearView(targetEl, year, cYear, cMonth, cDay) {
 
     template += "<li class='none item gray'>" + (decYears[0] - 1) + "</li>";
     for (var i = 0; i < decYears.length; i++) {
-        template += "<li class='click item' name='item-click' val='" + decYears[i] + "'>" + decYears[i] + "</li>";
+        var clazz = 'click item';
+        if (decYears[i] == cYear) {
+            clazz += " selected";
+        }
+        template += "<li class='" + clazz + "' name='item-click' val='" + decYears[i] + "'>" + decYears[i] + "</li>";
     }
     template += "<li class='none item gray'>" + (decYears[9] + 1) + "</li>";
 
@@ -133,17 +161,17 @@ function selectYearView(targetEl, year, cYear, cMonth, cDay) {
     var nextEl = document.getElementsByName('next')[0];
 
     preEl.addEventListener('click', function () {
-        selectYearView(targetEl, year - 10, cYear, cMonth, cDay);
+        selectYearView(el, targetEl, year - 10, cYear, cMonth, cDay);
     }, false);
 
     nextEl.addEventListener('click', function () {
-        selectYearView(targetEl, year + 10, cYear, cMonth, cDay);
+        selectYearView(el, targetEl, year - -10, cYear, cMonth, cDay);
     }, false);
 
     // 点击小条目的事件
     document.getElementsByName('item-click').forEach(function (itemNode) {
         itemNode.addEventListener('click', function () {
-            selectMonthView(targetEl, itemNode.getAttribute('val'), cYear, cMonth, cDay);
+            selectMonthView(el, targetEl, itemNode.getAttribute('val'), cYear, cMonth, cDay);
         }, false);
     });
 }
@@ -162,16 +190,25 @@ function selectYearView(targetEl, year, cYear, cMonth, cDay) {
 /**
  * 年月日日历控件
  * @param {element} el 挂载点，必输
- * @param {dateString} initValue 初始化日期，可选
  */
-function Calendar(el, initValue) {
+function Calendar(el) {
 
     var targetEl = document.createElement('div');
     targetEl.setAttribute('class', 'calendar-view');
 
     document.body.appendChild(targetEl);
 
-    // 初始化启动
-    selectDayView(targetEl, 2020, 8);
+    // 初始化挂载点击打开方法
+    el.addEventListener('click', function () {
+
+        var cDate = new Date(el.value);
+        if (cDate == 'Invalid Date') {
+            var todayDate = new Date();
+            selectDayView(el, targetEl, todayDate.getFullYear(), todayDate.getMonth() + 1, -1, -1, -1);
+        } else {
+            selectDayView(el, targetEl, cDate.getFullYear(), cDate.getMonth() + 1, cDate.getFullYear(), cDate.getMonth() + 1, cDate.getDate());
+        }
+
+    });
 
 };
